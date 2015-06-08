db991cc69a238128fe9f4165d0e8445582cb8a27
David P. Hanesana <dhanesana@gmail.com>
2015-06-07 22:57:25 -0700

    Add string interpolation

--- lib/plugins/kst.rb
+++ lib/plugins/kst.rb
@@ -4,12 +4,11 @@ class Kst
   match /(kst)$/, prefix: /^(\.)/
   match /(help kst)$/, method: :help, prefix: /^(\.)/
 
-
   def execute(m)
     utc = Time.now.utc
     kst = utc + (9 * 3600)
     time = kst.strftime("%F %H:%M KST")
-    m.reply time
+    m.reply "#{time}"
   end
 
   def help(m)
