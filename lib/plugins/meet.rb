f9cccc8133acc0c8d2d9039f34c5e92d68b1e0b5
David P. Hanesana <dhanesana@gmail.com>
2015-06-12 15:23:59 -0700

    Add page 2 of meet greet list

--- lib/plugins/meet.rb
+++ lib/plugins/meet.rb
@@ -11,14 +11,22 @@ class Meet
 
   def execute(m)
     feed = open("http://mwave.interest.me/meetgreet/list.json").read
+    feed_2 = open("http://mwave.interest.me/meetgreet/list.json?page=2").read
     result = JSON.parse(feed)
+    result_2 = JSON.parse(feed)
     today = (Time.now.utc - (07 * 3600)).strftime("%Y%m%d")
     meets = ''
+
     result['listMap'].each do |x|
       meets += x['TITLE'] if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
       meets += " #{(Time.strptime("#{x['START_TM']}", '%Q') + (16 * 3600)).strftime("%H:%M")}KST | " if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
     end
-    # m.reply "#{result['listMap'][0]['START_TM']}"
+
+    result_2['listMap'].each do |x|
+      meets += x['TITLE'] if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
+      meets += " #{(Time.strptime("#{x['START_TM']}", '%Q') + (16 * 3600)).strftime("%H:%M")}KST | " if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
+    end
+
     return m.reply "no scheduled meet & greets today bru" if meets.size < 1
     m.reply meets.join(', ')
   end
