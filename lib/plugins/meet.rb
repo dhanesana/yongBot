53f14dca58a11ed61ae94af0f66114cc7825cdcc
David P. Hanesana <dhanesana@gmail.com>
2015-06-12 14:49:54 -0700

    Add .meet command. Debugging

---
+++ lib/plugins/meet.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+require 'json'
+require 'time'
+
+class Meet
+  include Cinch::Plugin
+
+  match /(meet)$/, prefix: /^(\.)/
+  match /(help meet)$/, method: :help, prefix: /^(\.)/
+
+
+  def execute(m)
+    feed = open("http://mwave.interest.me/meetgreet/list.json").read
+    result = JSON.parse(feed)
+    today = Time.now.utc.strftime("%Y%m%d")
+    meets = ''
+    result['listMap'].each do |x|
+      meets += x['TITLE'] if Time.strptime("#{x['START_TM']}", '%Q').utc.strftime("%Y%m%d") == today
+      # meets += Time.strptime("#{x['START_TM']}", '%Q').utc.strftime("%H%m")
+    end
+    m.reply "#{result['listMap'][0]['START_TM']}"
+    return m.reply "no scheduled meet & greets today bru" if meets.size < 1
+    m.reply meets.join(', ')
+  end
+
+  def help(m)
+    m.reply "returns mwave meet & greets scheduled for the day"
+  end
+
+end
