fbeb2c38aa60b36de9d7a04f880a84b87a443b31
David P. Hanesana <dhanesana@gmail.com>
2015-06-12 16:24:56 -0700

    Modify .meet to return upcoming meet & greet

--- lib/plugins/meet.rb
+++ lib/plugins/meet.rb
@@ -15,24 +15,28 @@ class Meet
     result = JSON.parse(feed)
     result_2 = JSON.parse(feed)
     today = (Time.now.utc - (07 * 3600)).strftime("%Y%m%d")
-    meets = ''
+    meets = {}
 
     result['listMap'].each do |x|
-      meets += x['TITLE'] if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
-      meets += " #{(Time.strptime("#{x['START_TM']}", '%Q') + (16 * 3600)).strftime("%H:%M")}KST | " if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
+      meets["#{x['TITLE']}"] = (Time.strptime(x['START_TM'], '%Q') + (16 * 3600)).to_i unless DateTime.now.strftime('%Q').to_i > x['START_TM'].to_i
     end
 
     result_2['listMap'].each do |x|
-      meets += x['TITLE'] if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
-      meets += " #{(Time.strptime("#{x['START_TM']}", '%Q') + (16 * 3600)).strftime("%H:%M")}KST | " if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
+      meets["#{x['TITLE']}"] = (Time.strptime(x['START_TM'], '%Q') + (16 * 3600)).to_i unless DateTime.now.strftime('%Q').to_i > x['START_TM'].to_i
     end
 
+    # result_2['listMap'].each do |x|
+    #   meets += x['TITLE'] if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
+    #   meets += " #{(Time.strptime("#{x['START_TM']}", '%Q') + (16 * 3600)).strftime("%H:%M")}KST | " if (Time.strptime("#{x['START_TM']}", '%Q').strftime("%Y%m%d")) == today
+    # end
     return m.reply "no scheduled meet & greets today bru" if meets.size < 1
-    m.reply meets.join(', ')
+    meets = meets.sort_by { |title, date| date }
+    m.reply "Next - #{meets.first.first} #{Time.strptime(meets[meets.first.first].to_s, '%Q').strftime("%m/%d %H:%MKST")}"
+
   end
 
   def help(m)
-    m.reply "returns mwave meet & greets scheduled for the day"
+    m.reply "returns next scheduled mwave meet & greet"
   end
 
 end
