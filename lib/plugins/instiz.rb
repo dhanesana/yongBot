7851c45d9d050944768a4d816c1cee76cdc11679
David P. Hanesana <dhanesana@gmail.com>
2015-06-15 12:03:00 -0700

    Debug - convert num to integer

--- lib/plugins/instiz.rb
+++ lib/plugins/instiz.rb
@@ -12,10 +12,10 @@ class Instiz
     page = agent.get(
         'http://www.instiz.net/iframe_ichart_score.htm',
         nil, referer_url)
-    rank = num - 2
+    rank = num.to_i - 2
     one_song = page.at('div.ichart_score_song1').text
     one_artist = page.at('div.ichart_score_artist1').text
-    return m.reply "iChart Rank 1: #{one_song} by #{one_artist}" if num == 1
+    return m.reply "iChart Rank 1: #{one_song} by #{one_artist}" if num.to_i == 1
     title = page.parser.css('div.ichart_score2_song1')[rank].text
     artist = page.parser.css('div.ichart_score2_artist1')[rank].text
     m.reply "iChart Rank #{num}: #{title} by #{artist}"
