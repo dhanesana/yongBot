75a19887b6d45979e05a883ad3393563537ea102
David P. Hanesana <dhanesana@gmail.com>
2015-06-17 04:51:10 -0700

    Add easter egg response

--- lib/plugins/lyric.rb
+++ lib/plugins/lyric.rb
@@ -9,6 +9,7 @@ class Lyric
   def execute(m, command, lyric, keywords)
     RapGenius::Client.access_token = "#{ENV['RAPGENIUS']}"
     return m.reply 'no song found :(' if RapGenius.search_by_lyrics(lyric) == []
+    return m.reply "Corona - Rhythm of the Night https://youtu.be/u3ltZmI5LQw" if keywords.include? ('nike' && 'reebdok')
     song_id = RapGenius.search_by_lyrics(keywords).first.id
     song = RapGenius::Song.find(song_id)
     title = song.title
