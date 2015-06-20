36ee16432710eb6665758fb638009f2b0a2707a5
David P. Hanesana <dhanesana@gmail.com>
2015-06-20 14:30:54 -0700

    Debug typo - win_rate => win_ratio

--- lib/plugins/csgo.rb
+++ lib/plugins/csgo.rb
@@ -17,19 +17,19 @@ class Csgo
 
     total_k, total_d, hs, hit, fired, wins, rounds = 0, 0, 0, 0, 0, 0, 0
     result['playerstats']['stats'].each do |item|
-      total_k += item['value'] if item['name'] == 'total_kills'
-      total_d += item['value'] if item['name'] == 'total_deaths'
-      hs      += item['value'] if item['name'] == 'total_kills_headshot'
-      hit     += item['value'] if item['name'] == 'total_shots_hit'
-      fired   += item['value'] if item['name'] == 'total_shots_fired'
-      wins    += item['value'] if item['name'] == 'total_wins'
-      rounds  += item['value'] if item['name'] == 'total_rounds_played'
+      total_k += item['value'].to_f if item['name'] == 'total_kills'
+      total_d += item['value'].to_f if item['name'] == 'total_deaths'
+      hs      += item['value'].to_f if item['name'] == 'total_kills_headshot'
+      hit     += item['value'].to_f if item['name'] == 'total_shots_hit'
+      fired   += item['value'].to_f if item['name'] == 'total_shots_fired'
+      wins    += item['value'].to_f if item['name'] == 'total_wins'
+      rounds  += item['value'].to_f if item['name'] == 'total_rounds_played'
     end
 
     kd_ratio = (total_k / total_d).round(2)
     hs_ratio = ((hs / total_k) * 100).round(0)
     accuracy = ((hit / fired) * 100).round(0)
-    win_rate = ((wins / rounds) * 100).round(0)
+    win_ratio = ((wins / rounds) * 100).round(0)
 
     m.reply "K/D: #{kd_ratio} | HS: #{hs_ratio}% | ACC: #{accuracy}% | WIN: #{win_ratio}%"
   end
