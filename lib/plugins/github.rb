68c53abdc130ea1208b4c4663b1346b386649e1f
David P. Hanesana <dhanesana@gmail.com>
2015-06-26 22:43:52 -0700

    Add .github command

---
+++ lib/plugins/github.rb
@@ -0,0 +1,23 @@
+require 'httparty'
+require 'open-uri'
+
+class Github
+  include Cinch::Plugin
+
+  match /(github) (.+)/, prefix: /^(\.)/
+  match /(help github)$/, method: :help, prefix: /^(\.)/
+
+  def execute(m, command, github, user)
+    resp = HTTParty.get("https://api.github.com/users/#{user}/events")
+    message = resp.first['payload']['commits'].first['message']
+    url = resp.first['payload']['commits'].first['url']
+    resp_2 = HTTParty.get(url)
+    commit_url = resp_2['html_url']
+    m.reply "Last commit: #{message} | #{commit_url}"
+  end
+
+  def help(m)
+    m.reply 'returns most recent commit from specified github user'
+  end
+
+end
