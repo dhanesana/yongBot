408e3731f1dcd07e821771d6a3a383a1092736fc
David P. Hanesana <dhanesana@gmail.com>
2015-07-09 18:56:00 -0700

    Add additional gn image urls

--- lib/plugins/gn.rb
+++ lib/plugins/gn.rb
@@ -9,12 +9,21 @@ class Gn
     @gn = [
       "http://i.imgur.com/zseA3sP.jpg",
       "http://i.imgur.com/eT3TWtE.png",
-      "http://i.imgur.com/sJuqLKy.gif",
+      "http://i.imgur.com/sJuqLKy.gifv",
       "http://i.imgur.com/2qXK47K.jpg",
       "http://i.imgur.com/3Ct6vfM.jpg",
       "http://i.imgur.com/hQ4z6ug.png",
       "http://i.imgur.com/CjnuVw8.png",
-      "http://i.imgur.com/T4upJdE.jpg"
+      "http://i.imgur.com/T4upJdE.jpg",
+      "http://i.imgur.com/XAyCb4z.gifv",
+      "http://i.imgur.com/mhvU7fl.jpg",
+      "http://i.imgur.com/S3t7SFT.jpg",
+      "http://i.imgur.com/M9bmYe7.jpg",
+      "http://i.imgur.com/8npvAQi.jpg",
+      "http://i.imgur.com/awUeHzO.jpg",
+      "http://i.imgur.com/Gw0Xn7X.jpg",
+      "http://i.imgur.com/uho9c4M.png",
+      "http://i.imgur.com/rCN7cbH.jpg"
     ]
   end
 
