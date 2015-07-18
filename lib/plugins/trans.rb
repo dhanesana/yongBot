19068d3c141bb44f645be8d75a88b2d742c7da4f
David P. Hanesana <dhanesana@gmail.com>
2015-07-17 20:47:06 -0700

    Remove quotes

--- lib/plugins/trans.rb
+++ lib/plugins/trans.rb
@@ -12,7 +12,7 @@ class Trans
     response = Unirest.get("https://www.googleapis.com/language/translate/v2?key=#{ENV['GOOGLE']}&q=#{string}&target=en")
     translated = response.body['data']['translations'].first['translatedText']
     source_lang = response.body['data']['translations'].first['detectedSourceLanguage']
-    m.reply "Language: #{source_lang.upcase} => '#{translated}'"
+    m.reply "Language: #{source_lang.upcase} => #{translated}"
   end
 
   def help(m)
