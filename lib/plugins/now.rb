7b285f36ff7cac6a5348412a668451ea256d4f2d
David P. Hanesana <dhanesana@gmail.com>
2015-08-10 22:09:56 -0700

    Use different weather api

--- lib/plugins/now.rb
+++ lib/plugins/now.rb
@@ -8,27 +8,26 @@ class Now
   match /(help now)$/, method: :help, prefix: /^(\.)/
 
   def execute(m, command, now, location)
-    response = Unirest.get "https://montanaflynn-geocoder.p.mashape.com/address?address=#{URI.encode(location)}",
-      headers:{
-        "X-Mashape-Key" => "#{ENV['REK_MASHAPE']}",
-        "Accept" => "application/json"
-      }
-    return m.reply 'not a real place bru' if response.body.first.first == 'error'
-    # city = response.body['city']
-    # region = response.body['region']
-    # country = response.body['country']
-    lat = response.body['latitude']
-    long = response.body['longitude']
+    loc_array = location.split(/[[:space:]]/)
+    loc = loc_array.join(' ').downcase
+    if loc.to_i == 0
+      response = Unirest.get "http://api.openweathermap.org/data/2.5/weather?q=#{URI.encode(loc)}"
+    else
+      response = Unirest.get "http://api.openweathermap.org/data/2.5/weather?zip=#{loc.to_i}"
+    end
+    long = response.body['coord']['lon']
+    lat = response.body['coord']['lat']
+    kelvin = response.body['main']['temp'].to_f
+    temp = ((kelvin - 273.15) * 1.8000 + 32).round(2)
+    city = response.body['name']
+    country = response.body['sys']['country']
     response_2 = Unirest.get "http://api.timezonedb.com/?lat=#{lat}&lng=#{long}&format=json&key=#{ENV['TIMEZONE']}"
     time = response_2.body['timestamp'].to_s
-    response_3 = Unirest.get "http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}"
-    kelvin = response_3.body['main']['temp'].to_f
-    temp = ((kelvin - 273.15) * 1.8000 + 32).round(2)
-    m.reply "#{DateTime.strptime(time,'%s').strftime("%B %d, %Y %I:%M %p")} | Temp: #{temp} F"
+    m.reply "#{city}, #{country} | #{DateTime.strptime(time,'%s').strftime("%B %d, %Y %I:%M %p")} | Temp: #{temp} F"
   end
 
   def help(m)
-    m.reply 'returns current date, time, and temp for specified location'
+    m.reply 'returns current date, time, and temp for specified city or zip code'
   end
 
 end
