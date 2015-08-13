755c8a78b52678bb366839ff9fb5e8b8a2a03364
David P. Hanesana <dhanesana@gmail.com>
2015-08-12 21:29:18 -0700

    Update .cafe command

--- lib/plugins/cafe.rb
+++ lib/plugins/cafe.rb
@@ -1,34 +1,29 @@
-require 'nokogiri'
+require 'unirest'
 require 'open-uri'
 
 class Cafe
   include Cinch::Plugin
 
-  match /(cafe)$/, prefix: /^(\.)/
-  match /(cafe) (.+)/, method: :with_num, prefix: /^(\.)/
+  match /(cafe) (.+)/, prefix: /^(\.)/
   match /(help cafe)$/, method: :help, prefix: /^(\.)/
 
-  def execute(m)
-    page = Nokogiri::HTML(open('http://top.cafe.daum.net/_c21_/category_list?type=sub&subcateid=85&cateid=5'))
-    total = page.css('td.cafename').size
-    title = page.css('td.cafename')[0].children[1].text
-    url_array = page.css('td.cafename a.cafeinfo').map { |link| link['href'] }
-    score = page.css('td.score')[0].text
-    m.reply "Cafe Rank 1: #{title} #{url_array[0]} 점수: #{score}"
-  end
-
-  def with_num(m, command, cafe, num)
-    return m.reply 'invalid num bru' if num.to_i < 1
-    page = Nokogiri::HTML(open('http://top.cafe.daum.net/_c21_/category_list?type=sub&subcateid=85&cateid=5'))
-    total = page.css('td.cafename').size
-    title = page.css('td.cafename')[num.to_i - 1].children[1].text
-    url_array = page.css('td.cafename a.cafeinfo').map { |link| link['href'] }
-    score = page.css('td.score')[num.to_i - 1].text
-    m.reply "Rank #{num}: #{title} #{url_array[num.to_i - 1]} 점수: #{score}"
+  def execute(m, command, cafe, text)
+    query = text.split(/[[:space:]]/).join(' ').downcase
+    response = Unirest.post "https://apis.daum.net/search/cafe?apikey=#{ENV['DAUM_KEY']}&q=#{URI.encode(query)}&result=20&output=json"
+    cafe_hash = {}
+    return m.reply 'no results bru' if response.body['channel']['result'] == '0'
+    response.body['channel']['item'].each do |cafe|
+      if cafe_hash[cafe['cafeUrl']].nil?
+        cafe_hash[cafe['cafeUrl']] = 1
+      else
+        cafe_hash[cafe['cafeUrl']] += 1
+      end
+    end
+    m.reply cafe_hash.max_by {|k,v| v}.first
   end
 
   def help(m)
-    m.reply 'returns artist/group at specified cafe rank (http://goo.gl/MKg9ia)'
+    m.reply 'searches daumcafe posts and returns cafe url for most frequent '
   end
 
 end
