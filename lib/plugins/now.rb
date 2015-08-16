2965fb7ffa681732ab737a88e3f293e63d7d0b21
David P. Hanesana <dhanesana@gmail.com>
2015-08-16 11:30:27 -0700

    Update .now command with humidity and heat index values

--- lib/plugins/now.rb
+++ lib/plugins/now.rb
@@ -19,11 +19,14 @@ class Now
     lat = response.body['coord']['lat']
     kelvin = response.body['main']['temp'].to_f
     temp = ((kelvin - 273.15) * 1.8000 + 32).round(2)
+    humid = response.body['main']['humidity']
+    feels = -42.379 + 2.04901523*temp + 10.14333127*humid - 0.22475541*temp*humid - 6.83783*10**-3*temp**2 - 5.481717*10**-2*humid**2 +
+        1.22874*10**-3*temp**2*humid + 8.5282*10**-4*temp*humid**2 - 1.99*10**-6*temp**2*humid**2
     city = response.body['name']
     country = response.body['sys']['country']
     response_2 = Unirest.get "http://api.timezonedb.com/?lat=#{lat}&lng=#{long}&format=json&key=#{ENV['TIMEZONE']}"
     time = response_2.body['timestamp'].to_s
-    m.reply "#{city}, #{country} | #{DateTime.strptime(time,'%s').strftime("%B %d, %Y %I:%M %p")} | Temp: #{temp} F"
+    m.reply "#{city}, #{country} | #{DateTime.strptime(time,'%s').strftime("%B %d, %Y %I:%M %p")} | Temp: #{temp} F (Feels like #{feels.round(2)} F | Humidity: #{humid}%"
   end
 
   def help(m)
