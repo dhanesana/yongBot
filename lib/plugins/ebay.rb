5e7fab721c2caa54e83baa1c7dd1cd7931bdf795
David P. Hanesana <dhanesana@gmail.com>
2015-08-25 17:17:50 -0700

    Return error response if search fails

--- lib/plugins/ebay.rb
+++ lib/plugins/ebay.rb
@@ -12,6 +12,7 @@ class Ebay
     response = Unirest.get(
       "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=#{ENV['EBAY_ID']}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=#{URI.encode(query)}"
     )
+    return 'failed search bru' if response.body['findItemsByKeywordsResponse'].first['ack'].first == 'Failure'
     return 'no results match ur search bru' if response.body['findItemsByKeywordsResponse'].first['searchResult'].first['@count'] == '0'
     title = response.body['findItemsByKeywordsResponse'].first['searchResult'].first['item'].first['title'].first
     buy_url = response.body['findItemsByKeywordsResponse'].first['searchResult'].first['item'].first['viewItemURL'].first
