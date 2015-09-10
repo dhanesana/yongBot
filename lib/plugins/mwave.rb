8cdfcf8c50bbbc2a7daa169940c64c6832ef831e
David P. Hanesana <dhanesana@gmail.com>
2015-09-10 02:37:22 -0700

    Add date to replies

--- lib/plugins/mwave.rb
+++ lib/plugins/mwave.rb
@@ -11,27 +11,30 @@ class Mwave
   def execute(m)
     link = open("http://mwave.interest.me/mcountdown/vote/mcdChart.json").read
     result = JSON.parse(link)
-    rank = result['top3McountdownCharts'][0]['RANKING']
-    song = result['top3McountdownCharts'][0]['SONG_NM']
+    rank   = result['top3McountdownCharts'][0]['RANKING']
+    song   = result['top3McountdownCharts'][0]['SONG_NM']
     artist = result['top3McountdownCharts'][0]['artistInfos'].first['ARTIST_NM']
-    m.reply "Mwave Rank #{rank}: #{artist} - #{song}"
+    date   = result['mcountdownChartPrevNextDate']['votePeriod']
+    m.reply "Mwave Rank #{rank}: #{artist} - #{song} | #{date}"
   end
 
   def with_num(m, command, mwave, num)
     return m.reply 'invalid num bru' if num.to_i < 1
+    return m.reply 'less than 51 bru' if num.to_i > 50
     link = open("http://mwave.interest.me/mcountdown/vote/mcdChart.json").read
     result = JSON.parse(link)
     if num.to_i <= 3
-      rank = result['top3McountdownCharts'][num.to_i - 1]['RANKING']
-      song = result['top3McountdownCharts'][num.to_i - 1]['SONG_NM']
+      rank   = result['top3McountdownCharts'][num.to_i - 1]['RANKING']
+      song   = result['top3McountdownCharts'][num.to_i - 1]['SONG_NM']
       artist = result['top3McountdownCharts'][num.to_i - 1]['artistInfos'].first['ARTIST_NM']
-      m.reply "Mwave Rank #{rank}: #{artist} - #{song}"
+      date   = result['mcountdownChartPrevNextDate']['votePeriod']
     else
-      rank = result['othersMcountdownCharts'][num.to_i - 4]['RANKING']
-      song = result['othersMcountdownCharts'][num.to_i - 4]['SONG_NM']
+      rank   = result['othersMcountdownCharts'][num.to_i - 4]['RANKING']
+      song   = result['othersMcountdownCharts'][num.to_i - 4]['SONG_NM']
       artist = result['othersMcountdownCharts'][num.to_i - 4]['artistInfos'].first['ARTIST_NM']
-      m.reply "Mwave Rank #{rank}: #{artist} - #{song}"
+      date   = result['mcountdownChartPrevNextDate']['votePeriod']
     end
+      m.reply "Mwave Rank #{rank}: #{artist} - #{song} | #{date}"
   end
 
   def help(m)
