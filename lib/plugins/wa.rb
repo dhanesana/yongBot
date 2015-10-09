0a9a8b82fc522532406f361c931846615898643b
David P. Hanesana <dhanesana@gmail.com>
2015-10-09 05:01:44 -0700

    Cut bot response if length greater than 75 characters

--- lib/plugins/wa.rb
+++ lib/plugins/wa.rb
@@ -11,8 +11,16 @@ class Wa
     input_array = text.split(/[[:space:]]/)
     input = input_array.join(' ').downcase
     response = Nokogiri::XML(open("http://api.wolframalpha.com/v2/query?input=#{URI.encode(input)}&appid=#{ENV['WA_ID']}"))
-    interp = response.xpath('//plaintext').children[0]
-    result = response.xpath('//plaintext').children[1]
+    interp = response.xpath('//plaintext').children[0].text
+    if interp.size > 75
+      interp.slice! 75..-1
+      interp += "..."
+    end
+    result = response.xpath('//plaintext').children[1].text
+    if result.size > 75
+      result.slice! 75..-1
+      result += "..."
+    end
     m.reply "#{interp} => #{result}"
   end
 
