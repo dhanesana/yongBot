6b1daa78d98d15a1e0e8a137dccaac7dd2fad5cd
David P. Hanesana <dhanesana@gmail.com>
2015-10-09 05:05:27 -0700

    Check for and delete nbsp characters

--- lib/plugins/wa.rb
+++ lib/plugins/wa.rb
@@ -11,12 +11,12 @@ class Wa
     input_array = text.split(/[[:space:]]/)
     input = input_array.join(' ').downcase
     response = Nokogiri::XML(open("http://api.wolframalpha.com/v2/query?input=#{URI.encode(input)}&appid=#{ENV['WA_ID']}"))
-    interp = response.xpath('//plaintext').children[0].text
+    interp = response.xpath('//plaintext').children[0].text.split(/[[:space:]]/).join(' ')
     if interp.size > 75
       interp.slice! 75..-1
       interp += "..."
     end
-    result = response.xpath('//plaintext').children[1].text
+    result = response.xpath('//plaintext').children[1].text.split(/[[:space:]]/).join(' ')
     if result.size > 75
       result.slice! 75..-1
       result += "..."
