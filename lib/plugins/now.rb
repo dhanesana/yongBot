c46c26437b8f61f7acbc2948e9268aa604bb1921
David P. Hanesana <dhanesana@gmail.com>
2015-12-28 19:34:20 -0800

    Return calculated heat index only if temperature is above 79F

--- lib/plugins/now.rb
+++ lib/plugins/now.rb
@@ -22,13 +22,14 @@ module Cinch
         kelvin = response.body['main']['temp'].to_f
         temp = ((kelvin - 273.15) * 1.8000 + 32).round(2)
         humid = response.body['main']['humidity']
-        feels = -42.379 + 2.04901523*temp + 10.14333127*humid - 0.22475541*temp*humid - 6.83783*10**-3*temp**2 - 5.481717*10**-2*humid**2 +
-            1.22874*10**-3*temp**2*humid + 8.5282*10**-4*temp*humid**2 - 1.99*10**-6*temp**2*humid**2
+        feels = "(feels like #{(-42.379 + 2.04901523*temp + 10.14333127*humid - 0.22475541*temp*humid - 6.83783*10**-3*temp**2 - 5.481717*10**-2*humid**2 +
+                    1.22874*10**-3*temp**2*humid + 8.5282*10**-4*temp*humid**2 - 1.99*10**-6*temp**2*humid**2).round(2)} F tho) "
+        feels = "" if temp < 80
         city = response.body['name']
         country = response.body['sys']['country']
         response_2 = Unirest.get "http://api.timezonedb.com/?lat=#{lat}&lng=#{long}&format=json&key=#{ENV['TIMEZONE']}"
         time = response_2.body['timestamp'].to_s
-        m.reply "#{city}, #{country} | #{DateTime.strptime(time,'%s').strftime("%B %d, %Y %I:%M %p")} | Temp: #{temp} F (feels like #{feels.round(2)} F tho) | Humidity: #{humid}%"
+        m.reply "#{city}, #{country} | #{DateTime.strptime(time,'%s').strftime("%B %d, %Y %I:%M %p")} | Temp: #{temp} F #{feels}| Humidity: #{humid}%"
       end
 
       def help(m)
