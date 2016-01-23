6b906f91b0384bca5136600ff2d936a7b8b465b4
David P. Hanesana <dhanesana@gmail.com>
2016-01-23 12:33:15 -0800

    Allow users to search vine posts by hashtag

--- lib/plugins/vine.rb
+++ lib/plugins/vine.rb
@@ -1,4 +1,5 @@
 require 'unirest'
+require 'open-uri'
 
 module Cinch
   module Plugins
@@ -6,6 +7,7 @@ module Cinch
       include Cinch::Plugin
 
       match /(vine)$/
+      match /(vine) (.+)/, method: :with_tag
       match /(help vine)$/, method: :help
 
       def execute(m)
@@ -21,8 +23,23 @@ module Cinch
         m.reply "#{user}: #{desc} #{url}"
       end
 
+      def with_tag(m, prefix, vine, tag)
+        return m.reply "no spaces. one word tags bru" if tag.include? " "
+        response = Unirest.get "https://community-vineapp.p.mashape.com/timelines/tags/#{URI.encode(tag)}",
+          headers:{
+            "X-Mashape-Key" => "#{ENV['MASHAPE_KEY']}",
+            "Accept" => "application/json"
+          }
+        return m.reply "hashtag not found bru" if response.body['data']['count'] == 0
+        num = rand(1..response.body['data']['records'].size) - 1
+        url = response.body['data']['records'][num]['shareUrl']
+        user = response.body['data']['records'][num]['username']
+        desc = response.body['data']['records'][num]['description']
+        m.reply "#{user}: #{desc} #{url}"
+      end
+
       def help(m)
-        m.reply 'returns random popular vine post'
+        m.reply 'returns random popular vine post (or specify hashtag for random hashtag related post)'
       end
 
     end
