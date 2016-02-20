c4626fd8a47dcb13e50ec34f6ab55c6251c255c4
David P. Hanesana <dhanesana@gmail.com>
2016-02-19 22:53:07 -0800

    Update kmf plugin to also be able to return ticket availability

--- lib/plugins/kmf.rb
+++ lib/plugins/kmf.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
+require 'mechanize'
+require 'time'
 
 module Cinch
   module Plugins
@@ -7,6 +9,7 @@ module Cinch
       include Cinch::Plugin
 
       match /(kmf)$/
+      match /(kmf) (.+)/, method: :with_sect
       match /(help kmf)$/, method: :help
 
       def execute(m)
@@ -20,8 +23,37 @@ module Cinch
         m.reply "KMF #{year}: #{lineup.join(', ')}"
       end
 
+      def with_sect(m, prefix, kmf, sect)
+        input_array = sect.split(/[[:space:]]/)
+        input = input_array.join(' ').downcase
+        section = "  #{input}".downcase
+        agent = Mechanize.new
+        agent.get("https://ticket.koreatimes.com/member/login.html") do |page|
+          # LOGIN
+          login_page = page.form_with(:action => '/member/member_login_process.html?bURL=') do |form|
+            username_field = form.field_with(:name => "login_id")
+            username_field.value = ENV['KMF_LOGIN']
+            password_field = form.field_with(:name => "login_password")
+            password_field.value = ENV['KMF_PW']
+            form.submit
+          end
+        end
+        year = (Time.now.utc + Time.zone_offset('PDT')).strftime("%Y")
+        agent.get("https://ticket.koreatimes.com/ticket_#{year}/ticket.php?event_id=EV021")
+        count = 0
+        # p agent.page.parser.class
+        agent.page.parser.css('table#Table_01').first.css('tr td').each do |td|
+          count += 1 if td.text.split(/[[:space:]]/).join(' ').downcase != section
+          break if td.text.split(/[[:space:]]/).join(' ').downcase == section
+        end
+        return m.reply "invalid section bru" if count % 8 > 0
+        row = (count / 8) + 5
+        m.reply agent.page.parser.css('td font')[row].text
+      end
+
       def help(m)
         m.reply 'returns recent/upcoming lineup for korea times music festival'
+        m.reply 'returns ticket availability if section is specified'
       end
 
     end
