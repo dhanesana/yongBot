1e97d4b690f92d1f1d516cfbe07654e8f30de296
David P. Hanesana <dhanesana@gmail.com>
2016-03-03 18:32:29 -0800

    Specify a response for nonexistent/empty sub

--- lib/plugins/sub.rb
+++ lib/plugins/sub.rb
@@ -11,6 +11,7 @@ module Cinch
 
       def execute(m, prefix, sub, tag)
         response = HTTParty.get("http://www.reddit.com/r/#{tag.downcase}/new.json")
+        return m.reply "sub dont exist or it got no posts bru" if response['data']['children'] == []
         link = response['data']['children'].first['data']['url']
         title = url = response['data']['children'].first['data']['title']
         m.reply "r/#{tag}: #{title} #{link}"
