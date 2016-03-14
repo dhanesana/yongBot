725aa6b7866f57eecf0fa5c4f28c8ae1f50c2688
David P. Hanesana <dhanesana@gmail.com>
2016-03-14 02:41:59 -0700

    Update .iono plugin to return image urls

--- lib/plugins/iono.rb
+++ lib/plugins/iono.rb
@@ -7,8 +7,23 @@ module Cinch
       match /(help iono)$/, method: :help
 
       def execute(m)
-        yes = ['yes', 'yeah', 'yeee', 'ya', 'YES!'].sample
-        no = ['no', 'nah', 'nope', 'nooo', 'NO!'].sample
+
+        yes = [
+          "http://i.imgur.com/66VXA2d.gifv",
+          "http://i.imgur.com/jx2vBgN.gifv",
+          "http://i.imgur.com/jF8ZbQT.gifv",
+          "http://i.imgur.com/J976BH7.gifv",
+          "http://i.imgur.com/XGFQqN9.gifv"
+        ].sample
+
+        no = [
+          "http://i.imgur.com/zmqeBGt.gifv",
+          "http://i.imgur.com/xVCpmiC.gifv",
+          "http://i.imgur.com/9nIwoQy.gifv",
+          "http://i.imgur.com/CWlXGRZ.gifv",
+          "http://i.imgur.com/40gV6k2.gifv"
+        ].sample
+
         m.reply rand(0..1) == 0 ? yes : no
       end
 
