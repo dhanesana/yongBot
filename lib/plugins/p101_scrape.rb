6c7047de971063ca20e5fe3d7a5ef24be1c2e7ad
David P. Hanesana <dhanesana@gmail.com>
2016-03-25 09:24:47 -0700

    Update p101 plugin to reference most recent rankings page

--- lib/plugins/p101_scrape.rb
+++ lib/plugins/p101_scrape.rb
@@ -19,10 +19,12 @@ module Cinch
         return with_name(m, '.', 'p101', rank) if rank.to_i < 1
         return m.reply "there's only 101 bru" if rank.to_i > 101
         page = Nokogiri::HTML(open('http://mnettv.interest.me/produce101/rank.dbp'))
-        name = page.css('p.name')[rank.to_i - 1].text
-        agency = page.css('p.agency')[rank.to_i - 1].text
-        votes = page.css('p.voteCount')[rank.to_i - 1].text
-        href = page.css('div.thumb a')[rank.to_i - 1].first[1]
+        href = page.css('li.mn_04 dd').last.children.first.attributes['href'].value
+        recent_page = Nokogiri::HTML(open("http://mnettv.interest.me#{href}"))
+        name = recent_page.css('p.name')[rank.to_i - 1].text
+        agency = recent_page.css('p.agency')[rank.to_i - 1].text
+        votes = recent_page.css('p.voteCount')[rank.to_i - 1].text
+        href = recent_page.css('div.thumb a')[rank.to_i - 1].first[1]
         m.reply "Rank #{rank} - #{name}, Agency: #{agency}, Vote Count: #{votes} http://mnettv.interest.me/#{href}"
       end
 
@@ -30,8 +32,10 @@ module Cinch
         input_array = name.split(/[[:space:]]/)
         input = input_array.join(' ').downcase
         page = Nokogiri::HTML(open('http://mnettv.interest.me/produce101/rank.dbp'))
+        href = page.css('li.mn_04 dd').last.children.first.attributes['href'].value
+        recent_page = Nokogiri::HTML(open("http://mnettv.interest.me#{href}"))
         count = 0
-        page.css('p.name').each do |contestant|
+        recent_page.css('p.name').each do |contestant|
           count += 1 if contestant.text != input
           break if contestant.text == input
         end
