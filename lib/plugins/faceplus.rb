71a83eed8f5defc7374b9ec827273c7270e40a71
David P. Hanesana <dhanesana@gmail.com>
2016-09-12 12:53:35 -0700

    Add response for undetected face

--- lib/plugins/faceplus.rb
+++ lib/plugins/faceplus.rb
@@ -20,6 +20,7 @@ module Cinch
         return m.reply '500: error but not ur fault bru' if response.code == 500
         return m.reply '403: auth error bru' if response.code == 403
         return m.reply '502: server too busy bru' if response.code == 502
+        return m.reply 'no face detected bru' if response.body['face'] == []
         age = response.body['face'].first['attribute']['age']['value']
         range = response.body['face'].first['attribute']['age']['range']
         gender = response.body['face'].first['attribute']['gender']['value']
