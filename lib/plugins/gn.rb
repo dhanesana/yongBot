e6b6645cc86596504d50a93ba7cae978b698eeaf
David P. Hanesana <dhanesana@gmail.com>
2017-01-06 23:54:00 -0800

    Rehaul gn plugin to use database

--- lib/plugins/gn.rb
+++ lib/plugins/gn.rb
@@ -1,75 +1,50 @@
+require 'pg'
+require 'open-uri'
+
 module Cinch
   module Plugins
     class Gn
       include Cinch::Plugin
 
       match /(gn)$/
+      match /(addgn) (.+)/, method: :add
+      match /(delgn) (.+)/, method: :del
+      match /(who) (.+)/, method: :who
+      match /(gnban) (.+)/, method: :ban_status
       match /(help gn)$/, method: :help
+      match /(help who)$/, method: :help_who
+      match /(help addgn)$/, method: :help_add
+      match /(help delgn)$/, method: :help_del
+      match /(help gnban)$/, method: :help_ban
 
       def initialize(*args)
         super
         @gn_pairs = {}
-        @gn = [
-          "https://i.imgur.com/zseA3sP.jpg",
-          "https://i.imgur.com/eT3TWtE.png",
-          "https://i.imgur.com/sJuqLKy.gifv",
-          "https://i.imgur.com/2qXK47K.jpg",
-          "https://i.imgur.com/3Ct6vfM.jpg",
-          "https://i.imgur.com/hQ4z6ug.png",
-          "https://i.imgur.com/CjnuVw8.png",
-          "https://i.imgur.com/T4upJdE.jpg",
-          "https://i.imgur.com/XAyCb4z.gifv",
-          "https://i.imgur.com/mhvU7fl.jpg",
-          "https://i.imgur.com/S3t7SFT.jpg",
-          "https://i.imgur.com/M9bmYe7.jpg",
-          "https://i.imgur.com/8npvAQi.jpg",
-          "https://i.imgur.com/awUeHzO.jpg",
-          "https://i.imgur.com/Gw0Xn7X.jpg",
-          "https://i.imgur.com/uho9c4M.png",
-          "https://i.imgur.com/rCN7cbH.jpg",
-          "https://i.imgur.com/V2VIfhJ.jpg",
-          "https://i.imgur.com/WQVEyxu.jpg",
-          "https://i.imgur.com/ubFCnrw.jpg",
-          "https://i.imgur.com/7XHEqqa.jpg",
-          "https://i.imgur.com/LrgA6VG.jpg",
-          "https://i.imgur.com/S1P66fV.gifv",
-          "https://i.imgur.com/ow92Wv8.jpg",
-          "https://i.imgur.com/6cyvpFE.jpg",
-          "https://i.imgur.com/98CPRWN.jpg",
-          "https://i.imgur.com/7zxduO7.jpg",
-          "https://i.imgur.com/QTrb69y.jpg",
-          "https://i.imgur.com/N0lJJj0.jpg",
-          "https://i.imgur.com/hbWdKmy.jpg",
-          "https://i.imgur.com/06WFZku.jpg",
-          "https://i.imgur.com/deCEKeH.jpg",
-          "https://i.imgur.com/0HlUkPj.jpg",
-          "https://i.imgur.com/vy49uKb.jpg",
-          "https://i.imgur.com/tdVbV9f.jpg",
-          "https://i.imgur.com/ch8MgXg.jpg",
-          "https://i.imgur.com/kXOio8B.jpg",
-          "https://i.imgur.com/PfxpFDn.jpg",
-          "https://i.imgur.com/jLNfGr8.jpg",
-          "https://i.imgur.com/IYvy5Kn.jpg",
-          "https://i.imgur.com/0lGGgll.jpg",
-          "https://i.imgur.com/oiS3ruk.jpg",
-          "https://i.imgur.com/NpIgKu3.jpg",
-          "https://i.imgur.com/Xg5c1Tt.gifv",
-          "https://i.imgur.com/vezmTTh.jpg",
-          "https://i.imgur.com/uXenjEL.gifv",
-          "https://i.imgur.com/YYYP0wr.jpg",
-          "https://i.imgur.com/fKiTNnX.jpg",
-          "https://i.imgur.com/951IrGs.gifv",
-          "https://i.imgur.com/9cKgxOO.jpg",
-          "https://i.imgur.com/Gp4ivfB.jpg",
-          "https://i.imgur.com/T4vvtHY.jpg"
-        ]
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        begin
+          res = conn.exec_params("CREATE TABLE gn (prefix varchar, who varchar, link varchar);")
+          conn.exec("INSERT INTO gn (prefix, who, link) VALUES ('#{$master}', 'yein', 'https://i.imgur.com/zseA3sP.jpg');")
+        rescue PG::Error => pg_error
+          puts '*' * 50
+          puts "Table creation failed: #{pg_error.message}"
+        end
+        # banned users
+        begin
+          res = conn.exec_params("CREATE TABLE gnbanned (prefix varchar);")
+          conn.exec("INSERT INTO gnbanned (prefix) VALUES ('user/testban');")
+        rescue PG::Error => pg_error
+          puts '*' * 50
+          puts "Table creation failed: #{pg_error.message}"
+        end
       end
 
       def execute(m)
         if @gn_pairs.keys.include? m.prefix.match(/@(.+)/)[1]
           m.reply "u get wat u deserve: #{@gn_pairs[m.prefix.match(/@(.+)/)[1]]}"
         else
-          @gn_pairs[m.prefix.match(/@(.+)/)[1]] = @gn.sample
+          conn = PG::Connection.new(ENV['DATABASE_URL'])
+          entries = conn.exec("SELECT * FROM gn")
+          @gn_pairs[m.prefix.match(/@(.+)/)[1]] = entries.field_values('link').sample
           m.reply @gn_pairs[m.prefix.match(/@(.+)/)[1]]
           Timer(3600, options = { shots: 1 }) do |x|
             @gn_pairs.delete(m.prefix.match(/@(.+)/)[1])
@@ -77,8 +52,121 @@ module Cinch
         end
       end
 
+      def add(m, prefix, addgn, url)
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        search = conn.exec("SELECT * FROM gnbanned WHERE prefix='#{m.prefix.match(/@(.+)/)[1]}'")
+        if search.ntuples > 0
+          return m.reply 'ur banned from adding images bru'
+        else
+          entry_array = url.split(' ')
+          if entry_array.size > 2
+            return m.reply "no spaces in name or url. only space between name and url bru"
+          elsif entry_array.size < 2
+            return m.reply "missing name or url"
+          else
+            if entry_array[1].include? 'imgur'
+              if entry_array[1].include? 'http'
+                if entry_array[1].include? 'https'
+                  return m.reply 'name before url' if (entry_array.first =~ URI::regexp).nil? == false
+                  search = conn.exec("SELECT * FROM gn WHERE link='#{conn.escape_string(conn.escape_string(entry_array[1]))}'")
+                  return m.reply 'url already exists in database bru' if search.ntuples > 0
+                  conn.exec("INSERT INTO gn (prefix, who, link) VALUES ('#{m.prefix.match(/@(.+)/)[1]}', '#{conn.escape_string(entry_array.first)}', '#{conn.escape_string(entry_array[1])}');")
+                  return m.reply "#{entry_array[1]} is added to database"
+                else
+                  entry_array[1].sub!('http','https')
+                  return m.reply 'name before url' if (entry_array.first =~ URI::regexp).nil? == false
+                  search = conn.exec("SELECT * FROM gn WHERE link='#{conn.escape_string(conn.escape_string(entry_array[1]))}'")
+                  return m.reply 'url already exists in database bru' if search.ntuples > 0
+                  conn.exec("INSERT INTO gn (prefix, who, link) VALUES ('#{m.prefix.match(/@(.+)/)[1]}', '#{conn.escape_string(entry_array.first)}', #{conn.escape_string(entry_array[1])}');")
+                  return m.reply "#{entry_array[1]} is added to database"
+                end
+              else
+                return m.reply "url must contain https://"
+              end
+            else
+              return m.reply 'url must be hosted on imgur'
+            end
+          end
+        end
+      end
+
+      def del(m, prefix, delgn, url)
+        ops = Channel(m.channel.name).ops.map { |x| x.nick }
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        search = conn.exec("SELECT * FROM gn WHERE link='#{conn.escape_string(url)}';")
+        if m.prefix.match(/@(.+)/)[1] == $master
+          return m.reply "url doesn't exist in database bru" if search.ntuples < 1
+          conn.exec("DELETE FROM gn WHERE link='#{conn.escape_string(url)}';")
+          return User(m.user.nick).notice("#{url} is removed from database")
+        elsif m.prefix.match(/@(.+)/)[1] == search.field_values('prefix').first
+          return m.reply "url doesn't exist in database bru" if search.ntuples < 1
+          conn.exec("DELETE FROM gn WHERE link='#{conn.escape_string(url)}';")
+          return User(m.user.nick).notice("#{url} is removed from database")
+        elsif ops.include? m.user.nick
+          return m.reply "url doesn't exist in database bru" if search.ntuples < 1
+          conn.exec("DELETE FROM gn WHERE link='#{conn.escape_string(url)}';")
+          return User(m.user.nick).notice("#{url} is removed from database")
+        else
+          m.reply 'https://youtu.be/OBWpzvJGTz4'
+        end
+      end
+
+      def who(m, prefix, who, url)
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        search = conn.exec("SELECT * FROM gn WHERE link='#{conn.escape_string(url)}';")
+        return m.reply "link not found bru" if search.field_values('who').first.nil?
+        m.reply "#{search.field_values('who').first}"
+      end
+
+      def ban_status(m, prefix, gnban, user_prefix)
+        return m.reply "https://youtu.be/OBWpzvJGTz4" if user_prefix == $master
+        ops = Channel(m.channel.name).ops.map { |x| x.nick }
+        if m.prefix.match(/@(.+)/)[1] == $master
+          conn = PG::Connection.new(ENV['DATABASE_URL'])
+          search = conn.exec("SELECT * FROM gnbanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
+          if search.ntuples > 0
+            conn.exec("DELETE FROM gnbanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
+            return User(m.user.nick).notice("#{user_prefix} is UNbanned from adding gn urls")
+          else
+            conn.exec("INSERT INTO gnbanned (prefix) VALUES ('#{conn.escape_string(user_prefix)}');")
+            return User(m.user.nick).notice("#{user_prefix} is banned from adding gn urls")
+          end
+        end
+        if ops.include? m.user.nick
+          conn = PG::Connection.new(ENV['DATABASE_URL'])
+          search = conn.exec("SELECT * FROM gnbanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
+          if search.ntuples > 0
+            conn.exec("DELETE FROM gnbanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
+            return User(m.user.nick).notice("#{conn.escape_string(user_prefix)} is UNbanned from adding gn urls")
+          else
+            conn.exec("INSERT INTO gnbanned (prefix) VALUES ('#{conn.escape_string(user_prefix)}');")
+            return User(m.user.nick).notice("#{conn.escape_string(user_prefix)} is banned from adding gn urls")
+          end
+        else
+          m.reply 'https://youtu.be/OBWpzvJGTz4'
+        end
+      end
+
       def help(m)
-        m.reply "returns random gn pic (#{@gn.size} pics)"
+        m.reply "returns random gn pic via destiny"
+      end
+
+      def help_who(m)
+        m.reply 'finds and returns name associated with gn url'
+      end
+
+      def help_add(m)
+        m.reply 'adds image to gn database'
+        m.reply '.addgn [name] [url]'
+      end
+
+      def help_del(m)
+        m.reply 'deletes image from gn database (uploader, master, or op only)'
+      end
+
+      def help_ban(m)
+        m.reply 'master or op can ban or unban users by prefix'
+        m.reply '.gnban user/BANNEDUSER'
       end
 
     end
