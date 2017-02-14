092b7bf4893b1144bba10a767ca7db9bfc9c59f3
David P. Hanesana <dhanesana@gmail.com>
2017-02-13 18:15:45 -0800

    Modify response if no guests have been announced

--- lib/plugins/kpoppin.rb
+++ lib/plugins/kpoppin.rb
@@ -12,12 +12,12 @@ module Cinch
       def execute(m)
         page = Nokogiri::HTML(open('http://www.arirang.co.kr/Radio/Radio_MessageBoard.asp?PROG_CODE=RADR0143&MENU_CODE=101862&code=Be6'))
         text = page.css('tr.ntce td.subjt')[2].text
-        p text[0].to_i
         lineup = []
         page.css('tr.ntce td.subjt').each do |subject|
           lineup << subject.text if subject.text[0] == '0'
           lineup << subject.text if subject.text[0].to_i > 0
         end
+        lineup = ['No guests have been announced'] if lineup.size == 0
         m.reply "[#{lineup.reverse.join('], [')}] 12:00 ~ 14:00KST"
       end
 
