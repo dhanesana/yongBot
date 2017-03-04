60666ef687de25f56fe5d52b164edc409c7c13bc
David P. Hanesana <dhanesana@gmail.com>
2017-03-03 18:57:11 -0800

    Fix scrape selectors and refactor olleh plugin

--- lib/plugins/olleh.rb
+++ lib/plugins/olleh.rb
@@ -1,4 +1,5 @@
-require 'mechanize'
+require 'nokogiri'
+require 'open-uri'
 
 module Cinch
   module Plugins
@@ -15,24 +16,17 @@ module Cinch
 
       def with_num(m, prefix, olleh, num)
         return m.reply 'invalid num bru' if num.to_i < 1
-        return m.reply 'less than 51 bru' if num.to_i > 50
-        agent = Mechanize.new
-        referer_url = 'http://www.ollehmusic.com/'
-        page = agent.get(
-          "http://www.ollehmusic.com/Ranking/f_RealTimeRankingList.asp",
-          nil,
-          referer_url
-        )
-        title  = page.parser.css('a.titletxt')[num.to_i].text
-        artist = page.parser.css('p.artist a')[num.to_i - 1].text
-        date   = page.parser.css('div.time_1').first.text
-        time   = page.parser.css('div.cur_time p.current').text
-        time.slice!(-1)
-        m.reply "Olleh Rank #{num}: #{artist} - #{title} | #{date} #{time}:00KST"
+        return m.reply 'less than 11 bru' if num.to_i > 50
+        page = Nokogiri::HTML(open("https://www.ollehmusic.com/Ranking/f_RealTimeRankingList.asp"))
+        date = page.css('div.cur_date div.time_1').first.text.strip
+        time = page.css('div.cur_time').first.text.strip
+        artist = page.css('table.realtimechart p.artist a')[num - 1].text
+        title = page.css('table.realtimechart p.title a.titletxt')[num - 1].text
+        m.reply "Olleh Rank #{num}: #{artist} - #{title} | #{date} #{time}KST"
       end
 
       def help(m)
-        m.reply 'returns current song at specified olleh music rank'
+        m.reply 'returns current song at specified olleh realtime rank'
       end
 
     end
