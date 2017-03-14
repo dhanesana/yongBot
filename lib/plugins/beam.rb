178c2a48c8f67782b98793a0f363ae947fc48dfd
David P. Hanesana <dhanesana@gmail.com>
2017-03-13 19:09:42 -0700

    Use db to store beam users

--- lib/plugins/beam.rb
+++ lib/plugins/beam.rb
@@ -1,6 +1,7 @@
 require 'unirest'
 require 'open-uri'
 require 'pg'
+require 'pastebin-api'
 
 module Cinch
   module Plugins
@@ -10,17 +11,161 @@ module Cinch
       timer 120, method: :check_live
       match /(beam)$/
       match /(beam) (.+)/, method: :check_user
+      match /(beam) (list)$/, method: :list
+      match /(addbeam) (.+)/, method: :add_streamer
+      match /(delbeam) (.+)/, method: :del_streamer
+      match /(beamban) (.+)/, method: :ban_unban
+      match /(beamban) (list)$/, method: :ban_list
       match /(help beam)$/, method: :help
 
       def initialize(*args)
         super
-        @users = ENV['BEAM_USERS'].split(',')
         @online = []
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        banned_table(conn)
+        create_table(conn)
+      end
+
+      def create_table(conn)
+        # beam db
+        begin
+          res = conn.exec_params("create table beam (prefix varchar, streamer varchar);")
+          conn.exec(
+            "INSERT INTO beam (prefix, streamer) VALUES ('user/master', '1');"
+          )
+        rescue PG::Error => pg_error
+          puts '*' * 50
+          puts "Beam Table creation failed: #{pg_error.message}"
+        end
+        beam_streamers = conn.exec("SELECT * FROM beam;")
+        @streamers = []
+        beam_streamers.each do |row|
+          @streamers << row['streamer'].downcase
+        end
+      end
+
+      def banned_table(conn)
+        # banned users db
+        begin
+          res = conn.exec_params("CREATE TABLE beambanned (prefix varchar);")
+          conn.exec("INSERT INTO beambanned (prefix) VALUES ('user/testban');")
+        rescue PG::Error => pg_error
+          puts '*' * 50
+          puts "Table creation failed: #{pg_error.message}"
+        end
+        banned_users = conn.exec("SELECT * FROM beambanned;")
+        @banned = []
+        banned_users.each do |row|
+          @banned << row['prefix'].downcase
+        end
+      end
+
+      def ban_unban(m, prefix, gnban, user_prefix)
+        return if user_prefix == 'list'
+        # can't ban $master
+        return m.is_unauthorized if user_prefix == $master
+        return ban_toggle(m, user_prefix) if m.is_admin?
+        return ban_toggle(m, user_prefix) if m.is_op?
+        m.is_unauthorized
+      end
+
+      def ban_toggle(m, user_prefix)
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        search = conn.exec("SELECT * FROM beambanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
+        if search.ntuples > 0
+          conn.exec("DELETE FROM beambanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
+          return User(m.user.nick).notice("#{conn.escape_string(user_prefix)} is UNbanned from adding beam users")
+        else
+          conn.exec("INSERT INTO beambanned (prefix) VALUES ('#{conn.escape_string(user_prefix)}');")
+          return User(m.user.nick).notice("#{conn.escape_string(user_prefix)} is banned from adding beam users")
+        end
+        banned_users = conn.exec("SELECT * FROM beambanned;")
+        @banned = []
+        banned_users.each do |row|
+          @banned << row['prefix'].downcase
+        end
+      end
+
+      def ban_list(m)
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        pastebin = Pastebin::Client.new(ENV['PASTEBIN_KEY'])
+        string = ""
+        if m.is_admin?
+          get_all = conn.exec("SELECT * FROM beambanned;")
+          get_all.each do |x|
+            string += "#{x['prefix']}"
+            string += "\n"
+          end
+        else
+          m.is_unauthorized
+        end
+        # Unlisted paste titled '.beambanned list' expires in 10 minutes
+        m.user.msg(pastebin.newpaste(string, api_paste_name: '.beamban list', api_paste_private: 1, api_paste_expire_date: '10M'))
+        m.reply "check ur pms for list of beambanned prefixes"
+      end
+
+      def add_streamer(m, prefix, addbeam, streamer)
+        return m.reply 'registered users only bru' if m.user.host.include? 'Snoonet'
+        return m.is_unauthorized if @banned.include? m.user.host
+        return m.reply "#{streamer} already in db" if @streamers.include? streamer
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        conn.exec("INSERT INTO beam (prefix, streamer) VALUES ('#{m.user.host}', '#{conn.escape(streamer)}');")
+        beam_streamers = conn.exec("SELECT * FROM beam")
+        @streamers = []
+        beam_streamers.each do |row|
+          @streamers << row['streamer'].downcase
+        end
+        m.reply "#{streamer} added to beam db"
+      end
+
+      def del_streamer(m, prefix, delbeam, streamer)
+        return m.reply 'registered users only bru' if m.user.host.include? 'Snoonet'
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        search = conn.exec("SELECT * FROM beam WHERE streamer='#{conn.escape(streamer)}';")
+        return m.reply "streamer doesn't exist in database bru" if search.ntuples < 1
+        return del_streamer_db(m, conn, streamer) if m.is_admin?
+        return del_streamer_db(m, conn, streamer) if m.is_op?
+        return del_streamer_db(m, conn, streamer) if m.user.host == search.field_values('prefix').first
+        m.isunauthorized
+      end
+
+      def del_streamer_db(m, conn, streamer)
+        conn.exec("DELETE FROM beam WHERE streamer='#{conn.escape(streamer)}';")
+        beam_streamers = conn.exec("SELECT * FROM beam;")
+        @streamers = []
+        beam_streamers.each do |row|
+          @streamers << row['streamer'].downcase
+        end
+        m.reply "#{streamer} is removed from database"
+      end
+
+      def list(m)
+        return m.reply 'registered users only bru' if m.user.host.include? 'Snoonet'
+        return m.is_unauthorized if @banned.include? m.user.host
+        conn = PG::Connection.new(ENV['DATABASE_URL'])
+        pastebin = Pastebin::Client.new(ENV['PASTEBIN_KEY'])
+        string = ""
+        if m.is_admin?
+          get_all = conn.exec("SELECT * FROM beam ORDER BY prefix DESC;")
+          get_all.each do |x|
+            string += "'#{x['prefix']}' => 'https://beam.pro/#{x['streamer']}',"
+            string += "\n"
+          end
+        else
+          get_all = conn.exec("SELECT * FROM beam;")
+          get_all.each do |x|
+            string += "https://beam.pro/#{x['streamer']},"
+            string += "\n"
+          end
+        end
+        # Unlisted paste titled '.gn list' expires in 10 minutes
+        m.user.msg(pastebin.newpaste(string, api_paste_name: '.beam list', api_paste_private: 1, api_paste_expire_date: '10M'))
+        m.reply "check ur pms for list of saved beam users"
       end
 
       def execute(m)
         counter = 0
-        @users.each do |user|
+        @streamers.each do |user|
           user_get = Unirest.get "https://beam.pro/api/v1/channels/#{URI.encode(user)}"
           counter += 1 if user_get.body['online'] == false
           return m.reply "no1 streaming" if counter == @users.size
@@ -36,7 +181,7 @@ module Cinch
 
       def check_live
         response = "LIVE:"
-        @users.each do |user|
+        @streamers.each do |user|
           user_get = Unirest.get "https://beam.pro/api/v1/channels/#{URI.encode(user)}"
           @online.delete(user) if user_get.body['online'] == false
           next if user_get.body['online'] == false
@@ -54,6 +199,9 @@ module Cinch
       end
 
       def check_user(m, prefix, check_user, user)
+        return if user == 'list'
+        return m.reply 'registered users only bru' if m.user.host.include? 'Snoonet'
+        return m.is_unauthorized if @banned.include? m.user.host
         query = user.split(/[[:space:]]/).join(' ')
         user_get = Unirest.get "https://beam.pro/api/v1/channels/#{URI.encode(query)}"
         return m.reply "#{user} is not live bru" if user_get.body['online'] == false
