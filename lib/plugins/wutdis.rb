518ab9e9ce10dcc98eba03a74d9a868603729371
David P. Hanesana <dhanesana@gmail.com>
2017-04-14 01:34:43 -0700

    Additional response and refactor

--- lib/plugins/wutdis.rb
+++ lib/plugins/wutdis.rb
@@ -11,19 +11,14 @@ module Cinch
 
       def execute(m, prefix, wutdis, link)
         url = URI.encode(link)
-        response = Unirest.post "http://rekognition.com/func/api/?api_key=#{ENV['REKOGNITION_KEY']}&api_secret=#{ENV['REKOGNITION_SECRET']}&jobs=scene_understanding_3&urls=#{url}",
-          headers:{
-            "X-Mashape-Key" => "#{ENV['MASHAPE_KEY']}",
-            "Content-Type" => "application/x-www-form-urlencoded",
-            "Accept" => "application/json"
-          }
-
-        match1 = response.body['scene_understanding']['matches'].first['tag']
-        score1 = (response.body['scene_understanding']['matches'].first['score'].to_f * 100).round(2)
-        # match2 = response.body['scene_understanding']['matches'][1]['tag']
-        # score2 = " and maybe a #{(response.body['scene_understanding']['matches'][1]['score'].to_f * 100).round(2)}"
-
-        m.reply "looks like a #{match1.downcase}.. maybe. i'm #{score1.to_i}% sure tho.."
+        response = Unirest.post "http://rekognition.com/func/api/?api_key=#{ENV['REKOGNITION_KEY']}&api_secret=#{ENV['REKOGNITION_SECRET']}&jobs=scene_understanding_3&urls=#{url}"
+        match_one = response.body['scene_understanding']['matches'].first['tag']
+        score_one = (response.body['scene_understanding']['matches'].first['score'].to_f * 100).round(2)
+        if score_one.to_i >= 70
+          m.reply "looks like a #{match_one.downcase}.. i'm #{score_one}% sure!"
+        else
+          m.reply "looks like a #{match_one.downcase}.. maybe. i'm #{score_one.to_i}% sure"
+        end
       end
 
       def help(m)
