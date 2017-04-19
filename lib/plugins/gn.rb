0f85f143e0a6674157da45687907fbf8a791037e
David P. Hanesana <dhanesana@gmail.com>
2017-04-18 19:43:34 -0700

    Deprecate gnbanned table and refactor

--- lib/plugins/gn.rb
+++ lib/plugins/gn.rb
@@ -11,19 +11,20 @@ module Cinch
       match /(addgn) (.+)/, method: :add
       match /(delgn) (.+)/, method: :del
       match /(who) (.+)/, method: :who
-      match /(gnban) (.+)/, method: :ban_unban
-      match /(gnban) (list)$/, method: :ban_list
       match /(gn) (list)$/, method: :list
       match /(help gn)$/, method: :help
       match /(help who)$/, method: :help_who
       match /(help addgn)$/, method: :help_add
       match /(help delgn)$/, method: :help_del
-      match /(help gnban)$/, method: :help_ban
       match /(help gn list)$/, method: :help_list
 
       def initialize(*args)
         super
         @gn_pairs = {}
+        create_table
+      end
+
+      def create_table
         conn = PG::Connection.new(ENV['DATABASE_URL'])
         begin
           res = conn.exec_params("CREATE TABLE gn (prefix varchar, who varchar, link varchar);")
@@ -32,17 +33,10 @@ module Cinch
           puts '*' * 50
           puts "Table creation failed: #{pg_error.message}"
         end
-        # banned users
-        begin
-          res = conn.exec_params("CREATE TABLE gnbanned (prefix varchar);")
-          conn.exec("INSERT INTO gnbanned (prefix) VALUES ('user/testban');")
-        rescue PG::Error => pg_error
-          puts '*' * 50
-          puts "Table creation failed: #{pg_error.message}"
-        end
       end
 
       def execute(m)
+        return m.is_unauthorized if $banned.include? m.user.host
         return m.reply "u get wat u deserve: #{@gn_pairs[m.user.host]}" if @gn_pairs.keys.include? m.user.host
         conn = PG::Connection.new(ENV['DATABASE_URL'])
         entries = conn.exec("SELECT * FROM gn")
@@ -55,9 +49,7 @@ module Cinch
 
       def add(m, prefix, addgn, url)
         return m.reply 'registered users only bru' if m.user.host.include? 'Snoonet'
-        conn = PG::Connection.new(ENV['DATABASE_URL'])
-        search = conn.exec("SELECT * FROM gnbanned WHERE prefix='#{m.user.host}'")
-        return m.reply 'ur banned from adding images bru' if search.ntuples > 0
+        return m.is_unauthorized if $banned.include? m.user.host
         entry_array = url.split(' ')
         return m.reply "no spaces in name or url. only space between name and url bru" if entry_array.size > 2
         return m.reply "missing name or url" if entry_array.size < 2
@@ -77,6 +69,7 @@ module Cinch
       end
 
       def del(m, prefix, delgn, url)
+        return m.is_unauthorized if $banned.include? m.user.host
         conn = PG::Connection.new(ENV['DATABASE_URL'])
         search = conn.exec("SELECT * FROM gn WHERE link='#{conn.escape_string(url)}';")
         return m.reply "url doesn't exist in database bru" if search.ntuples < 1
@@ -92,52 +85,15 @@ module Cinch
       end
 
       def who(m, prefix, who, url)
+        return m.is_unauthorized if $banned.include? m.user.host
         conn = PG::Connection.new(ENV['DATABASE_URL'])
         search = conn.exec("SELECT * FROM gn WHERE link='#{conn.escape_string(url)}';")
         return m.reply "link not found bru" if search.field_values('who').first.nil?
         m.reply "#{search.field_values('who').first}"
       end
 
-      def ban_unban(m, prefix, gnban, user_prefix)
-        return if user_prefix == 'list'
-        # can't ban $master
-        return m.is_unauthorized if user_prefix == $master
-        return ban_toggle(m, user_prefix) if m.is_admin?
-        return ban_toggle(m, user_prefix) if m.is_op?
-        m.is_unauthorized
-      end
-
-      def ban_toggle(m, user_prefix)
-        conn = PG::Connection.new(ENV['DATABASE_URL'])
-        search = conn.exec("SELECT * FROM gnbanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
-        if search.ntuples > 0
-          conn.exec("DELETE FROM gnbanned WHERE prefix='#{conn.escape_string(user_prefix)}';")
-          return User(m.user.nick).notice("#{conn.escape_string(user_prefix)} is UNbanned from adding gn urls")
-        else
-          conn.exec("INSERT INTO gnbanned (prefix) VALUES ('#{conn.escape_string(user_prefix)}');")
-          return User(m.user.nick).notice("#{conn.escape_string(user_prefix)} is banned from adding gn urls")
-        end
-      end
-
-      def ban_list(m)
-        conn = PG::Connection.new(ENV['DATABASE_URL'])
-        pastebin = Pastebin::Client.new(ENV['PASTEBIN_KEY'])
-        string = ""
-        if m.is_admin?
-          get_all = conn.exec("SELECT * FROM gnbanned;")
-          get_all.each do |x|
-            string += "#{x['prefix']}"
-            string += "\n"
-          end
-        else
-          m.is_unauthorized
-        end
-        # Unlisted paste titled '.gnban list' expires in 10 minutes
-        m.user.msg(pastebin.newpaste(string, api_paste_name: '.gnban list', api_paste_private: 1, api_paste_expire_date: '10M'))
-        m.reply "check ur pms for list of gnbanned prefixes"
-      end
-
       def list(m)
+        return m.is_unauthorized if $banned.include? m.user.host
         conn = PG::Connection.new(ENV['DATABASE_URL'])
         pastebin = Pastebin::Client.new(ENV['PASTEBIN_KEY'])
         string = ""
@@ -174,11 +130,6 @@ module Cinch
 
       def help_del(m)
         m.reply 'deletes image from gn database (uploader, master, or op only)'
-      end
-
-      def help_ban(m)
-        m.reply 'master or op can ban or unban users by prefix'
-        m.reply '.gnban user/BANNEDUSER'
       end
 
       def help_list(m)
