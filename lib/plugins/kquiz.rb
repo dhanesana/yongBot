d445d0d2025f2f1a8132368998cc82cd6ea5160d
David P. Hanesana <dhanesana@gmail.com>
2017-05-19 11:09:01 -0700

    Check plural form of user guess

--- lib/plugins/kquiz.rb
+++ lib/plugins/kquiz.rb
@@ -82,12 +82,16 @@ module Cinch
         guess_words = []
         user_guess = words_only.split(/[[:space:]]/).join(' ').strip
         guess_words << user_guess
-        # Get singular of user_guess
-        page = Nokogiri::HTML(open("http://www.wordhippo.com/what-is/the-singular-of/#{user_guess}.html"))
-        if page.css('div.relatedwords b').first != nil
-          guess_words << page.css('div.relatedwords b').first.text.strip
+        # Get singular and plural of user_guess
+        get_singular = Nokogiri::HTML(open("http://www.wordhippo.com/what-is/the-singular-of/#{user_guess}.html"))
+        if get_singular.css('div.relatedwords b').first != nil
+          guess_words << get_singular.css('div.relatedwords b').first.text.strip
         end
-        # Check word and singular form
+        get_plural = Nokogiri::HTML(open("http://www.wordhippo.com/what-is/the-plural-of/#{user_guess}.html"))
+        if get_plural.css('div.relatedwords b').first != nil
+          guess_words << get_plural.css('div.relatedwords b').first.text.strip
+        end
+        # Check word, its singular, and its plural form
         count = 0
         guess_words.each do |word|
           if @all_games.keys.include? channel
