74a4563bff984b04837f64f568b721406510d7ad
David P. Hanesana <dhanesana@gmail.com>
2017-08-24 02:03:08 -0700

    Create YongIdentify module and prepend to Message class

--- lib/extensions/identify.rb
+++ lib/extensions/identify.rb
@@ -1,20 +1,27 @@
 require 'cinch'
 
+# User checks
+module YongIdentify
+  # Checks if message is from $master
+  def is_admin?
+    self.user.host == $master
+  end
+
+  # Checks if message is from a channel operator
+  def is_op?
+    ops = self.channel.ops.map { |usr| usr.host }
+    ops.include? self.user.host
+  end
+
+  # Response for unauthorized users
+  def is_unauthorized
+    reply "https://youtu.be/OBWpzvJGTz4"
+  end
+end
+
 module Cinch
+  # Prepend module to top-level class Message
   class Message
-
-    def is_admin?
-      self.user.host == $master
-    end
-
-    def is_op?
-      ops = self.channel.ops.map { |usr| usr.host }
-      ops.include? self.user.host
-    end
-
-    def is_unauthorized
-      self.reply "https://youtu.be/OBWpzvJGTz4"
-    end
-
+    prepend YongIdentify
   end
 end
