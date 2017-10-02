fc435ad4ef9a21f4ab02ba30b1d21deac26dbb0c
David P. Hanesana <dhanesana@gmail.com>
2017-10-02 16:06:04 -0700

    Update paste format

--- lib/plugins/twitch.rb
+++ lib/plugins/twitch.rb
@@ -83,18 +83,18 @@ module Cinch
         if m.is_admin?
           get_all = conn.exec("SELECT * FROM twitch ORDER BY prefix DESC;")
           get_all.each do |x|
-            string += "'#{x['prefix']}' => 'https://www.twitch.tv/#{x['streamer']}'"
+            string += "'#{x['prefix']}' => 'https://www.twitch.tv/#{x['streamer']}', "
             string += "\n"
           end
         else
           get_all = conn.exec("SELECT * FROM twitch;")
           get_all.each do |x|
-            string += "https://www.twitch.tv/#{x['streamer']}"
+            string += "https://www.twitch.tv/#{x['streamer']}, "
             string += "\n"
           end
         end
         # Unlisted paste titled '.gn list' expires in 10 minutes
-        m.user.msg(pastebin.newpaste(string, api_paste_name: '.twitch list', api_paste_private: 1, api_paste_expire_date: '10M'))
+        m.user.msg(pastebin.newpaste(string.chomp.chomp(', '), api_paste_name: '.twitch list', api_paste_private: 1, api_paste_expire_date: '10M'))
         m.reply "check ur pms for list of saved twitch users"
       end
 
