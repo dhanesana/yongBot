7967ee607d4af7eac258a35958e2c5c7cd0ebcbf
David P. Hanesana <dhanesana@gmail.com>
2017-10-11 15:37:05 -0700

    Add .mania album command to maniadb plugin

--- lib/plugins/maniadb.rb
+++ lib/plugins/maniadb.rb
@@ -7,7 +7,9 @@ module Cinch
       include Cinch::Plugin
 
       match /(maniadb) (.+)/
+      match /(mania album) (.+)/, method: :album_search
       match /(help maniadb)$/, method: :help
+      match /(help mania album)$/, method: :help_album
 
       def execute(m, prefix, maniadb, entry)
         query = entry.split(/[[:space:]]/).join(' ').downcase
@@ -19,8 +21,22 @@ module Cinch
         end
       end
 
+      def album_search(m, prefix, album_search, entry)
+        query = entry.split(/[[:space:]]/).join(' ').downcase
+        url = "http://dev.maniadb.com/index.php/api/search/#{URI.encode(query)}/?sr=album&display=1&key=example&v=0.5"
+        open(url) do |rss|
+          feed = RSS::Parser.parse(rss, false)
+          return m.reply '0 results' if feed.items == []
+          m.reply "#{feed.items.first.title}: #{feed.items.first.link}"
+        end
+      end
+
       def help(m)
         m.reply 'searches maniadb for specified artist page'
+      end
+
+      def help_album(m)
+        m.reply "searches maniadb for specified album page"
       end
 
     end
