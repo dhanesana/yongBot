40e6cf21bd28c8f117d091331761bc1f038445e6
David P. Hanesana <dhanesana@gmail.com>
2017-11-16 18:02:48 -0800

    Remove whitespace

--- lib/plugins/flickr.rb
+++ lib/plugins/flickr.rb
@@ -15,12 +15,12 @@ module Cinch
         nsid_req = open("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=#{ENV['FLICKR_KEY']}&username=#{username}&format=json&nojsoncallback=1").read
         result = JSON.parse(nsid_req)
         return m.reply "#{result[message]}" if result['stat'] == 'fail'
-        nsid = result['user']['nsid']
+        nsid = result['user']['nsid'].strip
         photos_req = open("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=#{ENV['FLICKR_KEY']}&user_id=#{nsid}&format=json&nojsoncallback=1").read
         response = JSON.parse(photos_req)
         return m.reply 'no photos found bru' if response['photos']['photo'].size == 0
-        photo_title = response['photos']['photo'].first['title']
-        photo_id = response['photos']['photo'].first['id']
+        photo_title = response['photos']['photo'].first['title'].strip
+        photo_id = response['photos']['photo'].first['id'].strip
         url = "https://www.flickr.com/photos/#{nsid}/#{photo_id}/"
         exif_req = JSON.parse(open("https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=#{ENV['FLICKR_KEY']}&photo_id=#{photo_id}&format=json&nojsoncallback=1").read)
         camera = ''
@@ -29,16 +29,16 @@ module Cinch
         focal = ''
         exif_req['photo']['exif'].each do |tag|
           if tag['label'] == "Model"
-            camera += tag['raw']['_content']
+            camera += tag['raw']['_content'].strip
           end
           if tag['label'] == "Focal Length"
-            focal += tag['clean']['_content']
+            focal += tag['clean']['_content'].strip
           end
           if tag['label'] == "Aperture"
-            aperture += tag['clean']['_content']
+            aperture += tag['clean']['_content'].strip
           end
           if tag['label'] == "Exposure"
-            exposure += tag['clean']['_content']
+            exposure += tag['clean']['_content'].strip
           end
         end
         m.reply "#{url} => #{camera} #{focal}, #{aperture}, #{exposure}"
