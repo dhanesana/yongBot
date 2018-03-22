76b4693c3c9fdbc83a0dc6837fcd33a4152ff6f6
David P. Hanesana <dhanesana@gmail.com>
2018-03-21 22:06:48 -0700

    Use begin-end block with response if there is an exception

--- lib/plugins/vapp.rb
+++ lib/plugins/vapp.rb
@@ -12,23 +12,27 @@ module Cinch
       match /(help vlive)$/, method: :help
 
       def execute(m)
-        page = Nokogiri::HTML(open("http://www.vlive.tv/upcoming", "User-Agent" => "Ruby/#{RUBY_VERSION}"))
-        return m.reply 'no live vapp streams bru' if page.css('li.on').size < 2
-        live = {}
-        page.css('li.on').each do |artist|
-          next if artist.css('em.title').text.strip == ''
-          live[artist.css('em.title').text.strip] = "http://www.vlive.tv#{artist.css('a').first['href']}"
-        end
-        if live.to_a.size < 4
-          live.to_a.each do |pair|
-            m.reply "#{pair[0]} => #{pair[1]}"
+        begin
+          page = Nokogiri::HTML(open("http://www.vlive.tv/upcoming", "User-Agent" => "Ruby/#{RUBY_VERSION}"))
+          return m.reply 'no live vapp streams bru' if page.css('li.on').size < 2
+          live = {}
+          page.css('li.on').each do |artist|
+            next if artist.css('em.title').text.strip == ''
+            live[artist.css('em.title').text.strip] = "http://www.vlive.tv#{artist.css('a').first['href']}"
           end
-        else
-          response = ""
-          live.to_a.each do |pair|
-            response += "[#{pair[0]} => #{pair[1]}], "
+          if live.to_a.size < 4
+            live.to_a.each do |pair|
+              m.reply "#{pair[0]} => #{pair[1]}"
+            end
+          else
+            response = ""
+            live.to_a.each do |pair|
+              response += "[#{pair[0]} => #{pair[1]}], "
+            end
+            m.reply response.chomp(', ')
           end
-          m.reply response.chomp(', ')
+        rescue Exception => e
+          return m.reply "Error: #{e}"
         end
       end
 
