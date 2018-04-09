6e2b2e351c4b627e9fb90f3736f9e1c6220c9757
David P. Hanesana <dhanesana@gmail.com>
2018-04-09 16:21:31 -0700

    Update response: URL to post regarding upcoming guests

--- lib/plugins/kpoppin.rb
+++ lib/plugins/kpoppin.rb
@@ -11,14 +11,19 @@ module Cinch
 
       def execute(m)
         page = Nokogiri::HTML(open("http://www.arirang.com/Radio/Radio_Announce.asp?PROG_CODE=RADR0143&MENU_CODE=101536&code=Be4"))
-        lineup = []
-        page.css('table.annlistTbl').first.css('td').each do |td|
-          next if td.text.include? 'PLAY MY STAR' # No guest appearance
-          next unless td.text.include? '('
-          lineup << td.text.strip
-        end
+        # lineup = []
+        # page.css('table.annlistTbl').first.css('td').each do |td|
+        #   next if td.text.include? 'PLAY MY STAR' # No guest appearance
+        #   next unless td.text.include? '('
+        #   lineup << td.text.strip
+        # end
         air_time = page.css('div.airtime p').first.text.strip + 'KST'
-        m.reply "[#{lineup.reverse.join('], [')}] #{air_time}"
+        # m.reply "[#{lineup.reverse.join('], [')}] #{air_time}"
+        href = "No upcoming guests announced"
+        page.css('table.annlistTbl a').each do |topic|
+          href = "http://www.arirang.com" + topic.values.first if topic.text.downcase.include? 'guest'
+        end
+        m.reply "[#{href}] #{air_time}"
       end
 
       def help(m)
